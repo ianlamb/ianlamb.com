e1a17646cbbca40a5f357162a39af306dcff6126
Ian Lamb <ianlamb32@gmail.com>
2022-03-03 21:45:43 -0800

    add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NotFoundPage() {
+    return (
+        <React.Fragment>
+            <h1>Page Not Found</h1>
+        </React.Fragment>
+    )
+}
