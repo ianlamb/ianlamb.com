4d4f85bad57463e08af4180797e4bedd1fe4b56e
Ian Lamb <ianlamb32@gmail.com>
2022-03-06 15:26:34 -0800

    fix ga tag

--- src/components/seo.js
+++ src/components/seo.js
@@ -50,14 +50,17 @@ const SEO = ({ title, description, image, article }) => {
                 href="https://fonts.googleapis.com/css2?family=Oswald&family=Roboto:ital,wght@0,400;0,700;1,400;1,700&display=swap"
                 rel="stylesheet"
             ></link>
-            <script>
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `
                 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
                 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
                 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
                 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
                 ga('create', 'UA-36320356-1', 'auto');
-                ga('send', 'pageview');
-            </script>
+                ga('send', 'pageview');`,
+                }}
+            ></script>
         </Helmet>
     )
 }
