0144b1f8a87d44410e9a076867b1c40966e4c9e3
Ian Lamb <ianlamb32@gmail.com>
2022-03-06 15:37:32 -0800

    update ga to new gtag

--- src/components/seo.js
+++ src/components/seo.js
@@ -50,15 +50,19 @@ const SEO = ({ title, description, image, article }) => {
                 href="https://fonts.googleapis.com/css2?family=Oswald&family=Roboto:ital,wght@0,400;0,700;1,400;1,700&display=swap"
                 rel="stylesheet"
             ></link>
+
+            <script
+                async
+                src="https://www.googletagmanager.com/gtag/js?id=G-WKHNZN4PXG"
+            ></script>
             <script
                 dangerouslySetInnerHTML={{
                     __html: `
-                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-                ga('create', 'UA-36320356-1', 'auto');
-                ga('send', 'pageview');`,
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                  
+                    gtag('config', 'G-WKHNZN4PXG');`,
                 }}
             ></script>
         </Helmet>
